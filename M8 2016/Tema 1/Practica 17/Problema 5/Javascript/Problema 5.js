5ef29c75db24aced54b983c3243bed1314bba8bf
Soreoniem <j_l_juan@hotmail.com>
2016-10-11 15:42:51 +0200

    + Ejercicio 5 practica 17
    • Añadido el ejercicio 5 de la practica 17

---
+++ M8 2016/Tema 1/Practica 17/Problema 5/Javascript/Problema 5.js
@@ -0,0 +1,27 @@
+﻿// aleatorio de 1 a 100 = ((0→99) +1)
+	// var aleatorio = parseInt(Math.round(Math.random() * (100 - 1) + 1));
+
+/*
+	Descripción de la práctica
+	► Crea un programa que permita cargar una dirección de email e
+		implementar una función que verifique si el String contiene
+		el caracter @
+	[] Pedir email
+	[] Verificar email
+*/
+function ejecutarProblema5(){
+	var email;
+	var emailValido = false;
+	var frase = "Correo electrónico:";
+	do{
+		email = prompt( frase, "eample@correo.com") + "";
+		
+		if( email.indexOf("@") != -1 ){
+			emailValido = true;
+		} else {
+			frase = "Problema: el correo electronico necesita una \"@\"";
+		}
+	}while(! emailValido);
+	
+	alert("Correo valido: "+ email);
+}
