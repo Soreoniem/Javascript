3457836d0c94ea78aefbe063b8667c78e30adfe4
Soreoniem <j_l_juan@hotmail.com>
2016-10-27 18:29:19 +0200

    Cabios forEach
    nueva versión

--- M8 2016/Tema 1/Practica forEach/Javascript/Practica forEach.js
+++ M8 2016/Tema 1/Practica forEach/Javascript/Practica forEach.js
@@ -7,9 +7,17 @@
 */
 
 
-var trabajos = ["1r Nombre", "2º Nombre", "3r Nombre", "4º Nombre"];
+var nombres = new Array();
+
+function obtenerDatos(){
+	var inputs = document.getElementsByTagName("input");
+	
+	for( var i=0 ; i<inputs.length ; i++ ){
+		nombres.push( document.getElementById("nom"+ (i +1)).value );
+	}
+}
 
 function Organizar(arrTraI, i) {
 	
-	document.getElementById("imprimirDatos").innerHTML += arrTraI +": <span style=\"color: red;\">"+ document.getElementById("nom"+ (i +1)).value +"</span><br/>";
+	document.getElementById("imprimirDatos").innerHTML += "<span style=\"color: red;\">"+ arrTraI +"</span><br/>";
 }
