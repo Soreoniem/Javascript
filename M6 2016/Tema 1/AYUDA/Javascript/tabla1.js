3889fb1a4b148b9be5758d08bc3842b717878091
Soreoniem <j_l_juan@hotmail.com>
2016-12-29 13:18:16 +0100

    Subida de precaución
    He de formatear el pc
    
    Pendiente de reisión

---
+++ M6 2016/Tema 1/AYUDA/Javascript/tabla1.js
@@ -0,0 +1,37 @@
+﻿var dia = ["hora", "lunes", "martes", "miercoles", "jueves", "viernes"];
+
+function generarTablero(horaMinima, horaMaxima){
+	var tabla = "";
+	
+	tabla += "<table>";
+	
+	// Primer for que crea la cabecera (variable dia)
+	tabla += "<tr>";
+	for( var i=0  ; i<dia.length ; i++ ){
+		if( i > 0 ){
+			tabla += "<td class=\"miClase\">"+ dia[i] +"</td>";
+		} else {
+			tabla += "<td>"+ dia[i] +"</td>";
+		}
+	}
+	tabla += "<tr>";
+	
+	// Segundo for para los datos
+	for (var j = horaMinima; j <= horaMaxima; j++){
+	
+		tabla += "<tr>";
+		
+		for (var n = 0; n < dia.length; n++){
+			if( n == 0 ){
+				tabla += "<td>"+ j +":00</td>";
+			} else {
+				tabla += "<td><input type=\"text\"></td>";
+			}
+		}
+		
+		tabla += "</tr>";
+	}
+	
+	tabla += "</table>";
+	document.getElementById("tabla").innerHTML = tabla;
+}
